5c538ba0ea316fd7ad2620d9f268970f031026c0
jschuhmann47 <jschuhmann@frba.utn.edu.ar>
2022-04-17 00:18:58 -0300

    mejora con biblioteca ffmpeg

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from __future__ import unicode_literals
 import youtube_dl
+import ffmpeg
+
+
 
 ydl_opts = {
     'format': 'bestaudio/best',
+    'outtmpl': 'output.%(ext)s',
     'postprocessors': [{
         'key': 'FFmpegExtractAudio',
         'preferredcodec': 'wav',
@@ -11,7 +15,9 @@ ydl_opts = {
     #'progress_hooks': [my_hook],
 }
 with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-    url=input("Ingresar URL de Youtube para convertir a wav: ")
+    url=input("Enter Youtube URL: ")
     ydl.download([url])
+    stream = ffmpeg.input('output.m4a')
+    stream = ffmpeg.output(stream, 'output.wav')
 
 #a
